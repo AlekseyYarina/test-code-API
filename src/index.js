591b44ac73eef304e0241dee8c12bebe95ae1e25
AlekseyYarina <aleksey.yarina@gmail.com>
2023-11-24 20:26:44 +0200

    upd

--- src/index.js
+++ src/index.js
@@ -1,20 +1,2 @@
-import { getProductsByParams } from './img-api';
-
-const fetchData = async () => {
-  const params = {
-    keyword: '',
-    category: 'Fresh_Produce',
-    page: 3,
-  };
-
-  try {
-    const data = await getProductsByParams(params);
-    console.log(data);
-    console.log(data.page);
-    console.log(data.totalPages);
-  } catch (error) {
-    console.error('Ошибка получения данных', error);
-  }
-};
-
-fetchData();
+import Pagination from 'tui-pagination';
+import 'tui-pagination/dist/tui-pagination.css';
